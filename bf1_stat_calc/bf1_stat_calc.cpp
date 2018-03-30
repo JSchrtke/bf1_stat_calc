5770ca1976592730b60d794df2480f417fe3272e
Johannes Schrötke <johannes.schroetke@gmail.com>
2018-03-30 16:25:52 +0200

    updated concept.txt

--- bf1_stat_calc/bf1_stat_calc.cpp
+++ bf1_stat_calc/bf1_stat_calc.cpp
@@ -1,7 +1,8 @@
 ﻿// bf1_stat_calc.cpp
 /*
 TO DO:
-- Figure out how to make random_number_max a double without messing up the random_number function
+- Figure out how to make random_number_max a double without messing up the
+ random_number function
 */
 #include "stdafx.h"
 #include "random_number.h"
@@ -9,8 +10,9 @@ TO DO:
 #include <ctime>
 
 int main()
-{	
-	int random_number_max; //maximum number for the random_number function; it generates a number from 0 to this value
+{
+	int random_number_max; /*maximum number for the random_number function;
+													it generates a number from 0 to this value*/
 	std::cin >> random_number_max;
 	std::cout << random_number(random_number_max) << std::endl;
     return 0;

34840d6cb1e92b805b61d52df7e6bd3441c3b564
Johannes Schrötke <johannes.schroetke@gmail.com>
2018-03-30 23:33:05 +0200

    added function to calculate the hrec offset for the spread circle

--- bf1_stat_calc/bf1_stat_calc.cpp
+++ bf1_stat_calc/bf1_stat_calc.cpp
@@ -5,6 +5,7 @@ TO DO:
 #include "stdafx.h"
 #include "random_number.h"
 #include "dist_betw_points.h"
+#include "hrec.h"
 #include <iostream>
 
 int main()
@@ -23,11 +24,20 @@ int main()
 	//radius of the spread
 	double spread_radius = 1.0;
 
-	double dist_betw_circle;
+	//distance to target
+	double distance = 10.0;
+
+	//magnitude of hrec
+	double hrec_magnitude;
+	double hrec_value_min = -0.24;
+	double hrec_value_max = 0.24;
+
 	//min/max number for random number generator, generates random double between those
 	double rand_num_min = 0.0;
 	double rand_num_max = 1.0;
 
+	hrec_magnitude = random_number(hrec_value_min, hrec_value_max);
+	std::cout << hrec(distance, hrec_magnitude) << std::endl;
 	std::cout << dist_betw_points(target_position_x, target_postion_y, spread_postion_x, spread_postion_y) << std::endl;
 	std::cout << random_number(rand_num_min, rand_num_max) << std::endl;
 	return 0;
---
+++ bf1_stat_calc/hrec.cpp
@@ -0,0 +1,13 @@
+﻿#include "hrec.h"
+#include "stdafx.h"
+#include "hrec.h"
+#include <iostream>
+
+const double PI = 3.141592653589793238463;
+
+double hrec(double distance, double hrec_magnitude)
+{
+	double hrec_hor_shift;
+	hrec_hor_shift = tan(hrec_magnitude * (PI / 180)) * distance;
+	return hrec_hor_shift;
+}

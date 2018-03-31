e62f0109d933af186cf570410970e24d76d27f12
Johannes Schrötke <johannes.schroetke@gmail.com>
2018-03-31 18:45:27 +0200

    added variables for weapon stats

--- bf1_stat_calc/bf1_stat_calc.cpp
+++ bf1_stat_calc/bf1_stat_calc.cpp
@@ -1,6 +1,7 @@
 ﻿// bf1_stat_calc.cpp
 /*
 TO DO:
+-add weapon stat variables
 */
 #include "stdafx.h"
 #include "random_number.h"
@@ -29,16 +30,27 @@ int main()
 
 	//magnitude of hrec
 	double hrec_magnitude;
-	double hrec_value_min = -0.24;
-	double hrec_value_max = 0.24;
+	
+	//Weapon stat variables
+	int rpm = 359;
+	int muz_vel = 660;
+	int mag_size = 5;
+	float deploy_time = 0.7;
+	float hor_recoil_r = 0.15;
+	float hor_recoil_l = -0.15;
+	float sips = 0.062;
+	float fssm_ads = 1;
+	float fssm_hip = 1;
+	float spread_dec = 3.75;
+	float spread_ads_not_move = 0.12;
+	float spread_ads_move = 0.82;
+	float spread_hip_stand_not_move = 2;
+	float spread_hip_crouch_not_move = 1.5;
+	float spread_hip_prone_not_move = 1;
+	float spread_hip_stand_move = 2.5;
+	float spread_hip_crouch_move = 2;
+	float spread_hip_prone_move = 1.5;
 
-	//min/max number for random number generator, generates random double between those
-	double rand_num_min = 0.0;
-	double rand_num_max = 1.0;
 
-	hrec_magnitude = random_number(hrec_value_min, hrec_value_max);
-	std::cout << hrec(distance, hrec_magnitude) << std::endl;
-	std::cout << dist_betw_points(target_position_x, target_postion_y, spread_postion_x, spread_postion_y) << std::endl;
-	std::cout << random_number(rand_num_min, rand_num_max) << std::endl;
 	return 0;
 }
